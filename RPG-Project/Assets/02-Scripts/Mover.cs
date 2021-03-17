cd231683749679c16c73baa81c166482c89584cc
Alessio Ebani <ebani.alessio@studenti-ittfedifermi.edu.it>
2021-03-17 17:28:59 +0100

    Set up a follow camera

--- RPG-Project/Assets/02-Scripts/Mover.cs
+++ RPG-Project/Assets/02-Scripts/Mover.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour {
-    [SerializeField] Transform target;
 
     // Update is called once per frame
     void Update() {
