51983516f5774128641e6cae6d1751c0fdf589c9
Alessio Ebani <ebani.alessio@studenti-ittfedifermi.edu.it>
2021-03-17 13:54:47 +0100

    Mouse movement with raycast

--- RPG-Project/Assets/02-Scripts/Mover.cs
+++ RPG-Project/Assets/02-Scripts/Mover.cs
@@ -5,14 +5,22 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour {
     [SerializeField] Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update() {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0)) {
+            MoveToCursor();
+        }
+        
+    }
+
+    private void MoveToCursor() {
+        //lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit=Physics.Raycast(ray, out hit);
+        if (hasHit) {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
